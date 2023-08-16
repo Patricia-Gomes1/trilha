d70b264f81d611dcee887799acd5031a50293810
Patricia-Gomes1 <patricia.gomes1@hotmail.com>
2023-08-16 19:06:14 -0300

    Exemplos da Aula

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        // final não deixa o valor ser alterado a variavel deverá ser em CAIXA ALTA
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
