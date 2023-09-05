2dc7ebb56b120eab15dc439bf127bd436fdbb594
Patricia-Gomes1 <patricia.gomes1@hotmail.com>
2023-09-05 18:22:00 -0300

    aulas operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,22 +1,120 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        String concatenacao = "?";
+        /*
+         * String concatenacao = "?";
+         * 
+         * concatenacao = 1 + 1 + 1 + "1";
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = 1 + "1" + 1 + 1;
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = 1 + "1" + 1 + "1";
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = "1" + 1 + 1 + 1;
+         * System.out.println(concatenacao);
+         * 
+         * concatenacao = "1" + (1 + 1 + 1);
+         * System.out.println(concatenacao);
+         * 
+         * .............................................................................
+         * 
+         * int numero = 5;
+         * System.out.println(-numero);
+         * 
+         * numero = -numero;
+         * System.out.println(numero);
+         * 
+         * numero = +numero * -1; // usar para transformar em numero positivo)
+         * System.out.println(numero);
+         * 
+         * numero = numero + 1;
+         * System.out.println(numero);
+         * 
+         * numero++;
+         * System.out.println(numero);
+         * 
+         * System.out.println(numero++); //assim ele soma só depois de impresso
+         * 
+         * System.out.println(++numero);
+         * 
+         * System.out.println(numero--);
+         * 
+         * System.out.println(numero);
+         * 
+         * .............................................................................
+         * 
+         * boolean variavel = true;/
+         * 
+         * System.out.println(!variavel);
+         * 
+         * variavel = !variavel; // inverte o valor de uma variavel booleana
+         * System.out.println(variavel);
+         * 
+         * .............................................................................
+         * 
+         * int a, b;
+         * 
+         * a = 6;
+         * b = 6;
+         * 
+         * String resultado = a == b ? "verdadeiro" : "falso";
+         * 
+         * EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+         * if (a == b)
+         * resultado = "verdadeiro";
+         * else
+         * resultado = "falso";
+         * 
+         * 
+         * System.out.println(resultado);
+         * ...............................................................
+         * 
+         * int numero1 = 1;
+         * int numero2 = 2;
+         * 
+         * boolean simNao = numero1 == numero2;
+         * 
+         * if (numero1 == numero2) {
+         * System.out.println("a nossa condição é verdadeira");
+         * }
+         * 
+         * System.out.println("NumeroUm é igual a Numero2? " + simNao);
+         * 
+         * simNao = numero1 != numero2;
+         * 
+         * System.out.println("NumeroUm é diferente a Numero2? " + simNao);
+         * 
+         * simNao = numero1 > numero2;
+         * 
+         * System.out.println("NumeroUm é maior que Numero2? " + simNao);
+         * 
+         * String nomeUm = "PATRICIA";
+         * String nomeDois = new String("PATRICIA");
+         * 
+         * System.out.println(nomeUm == nomeDois);
+         * System.out.println(nomeUm.equals(nomeDois)); //objetos, conteudos, usar o
+         * equals
+         * ..........................................................................
+         * 
+         * boolean condicao1 = true;
+         * boolean condicao2 = true;
+         * 
+         * if (condicao1 && condicao2) { // e
+         * System.out.println("As duas condições são verdadeiras ");
+         * }
+         * if (condicao1 || condicao2) { // ou
+         * System.out.println("Uma das duas condições é verdadeira ");
+         * }
+         * if (condicao1 && (7 > 4)) { // e
+         * System.out.println("As duas condições são verdadeiras ");
+         * }
+         * System.out.println("fim ");
+         * ................................................
+         */
 
-        concatenacao = 1 + 1 + 1 + "1";
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + 1;
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + "1";
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + 1 + 1 + 1;
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + (1 + 1 + 1);
-        System.out.println(concatenacao);
     }
 
 }
